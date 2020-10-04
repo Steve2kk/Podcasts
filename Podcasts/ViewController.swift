a540b9a62ee40868d90c8b0ff85e802255c696e1
Steve <steveshelaiev99@gmail.com>
2020-10-04 14:08:06 +0300

    Initial Commit

---
+++ Podcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by Vsevolod Shelaiev on 04.10.2020.
+//  Copyright © 2020 Vsevolod Shelaiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
